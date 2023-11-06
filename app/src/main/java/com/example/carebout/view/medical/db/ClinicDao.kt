f1a5a041e853f78fc214f6b7c095b97040244a14
Ahyun <jah29741@gmail.com>
2023-11-07 00:04:20 +0900

    medical - 검진 태그 목록 및 쿼리 추가

--- app/src/main/java/com/example/carebout/view/medical/db/ClinicDao.kt
+++ app/src/main/java/com/example/carebout/view/medical/db/ClinicDao.kt
@@ -23,6 +23,28 @@ interface ClinicDao {
     @Query("SELECT * FROM table_clinic WHERE clinicId = :id")
     fun getClinicById(id: Int): Clinic?
 
+    @Query("SELECT * FROM table_clinic ORDER BY date DESC")
+    fun getClinicDateAsc(): List<Clinic>
+    //ASC - 오름
+
+    @Query("SELECT * FROM table_clinic WHERE tag_blood = 1")
+    fun getClinicWithTagB(): List<Clinic>
+
+    @Query("SELECT * FROM table_clinic WHERE tag_xray = 1")
+    fun getClinicWithTagX(): List<Clinic>
+
+    @Query("SELECT * FROM table_clinic WHERE tag_ultrasonic = 1")
+    fun getClinicWithTagU(): List<Clinic>
+
+    @Query("SELECT * FROM table_clinic WHERE tag_ct = 1")
+    fun getClinicWithTagC(): List<Clinic>
+
+    @Query("SELECT * FROM table_clinic WHERE tag_mri = 1")
+    fun getClinicWithTagM(): List<Clinic>
+
+    @Query("SELECT * FROM table_clinic WHERE tag_checkup = 1")
+    fun getClinicWithTagCheckup(): List<Clinic>
+
     //@Query("DELETE FROM User WHERE name = :name") // 'name'에 해당하는 유저를 삭제해라
     //    fun deleteUserByName(name: String)
 }
