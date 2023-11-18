ce5846b809e9b4cdaf63e5aae827d0369a408750
dalyeon2 <woohyun2228@gmail.com>
2023-11-19 03:28:36 +0900

    Fix for DB

--- app/src/main/java/com/example/carebout/view/community/AddActivity.kt
+++ app/src/main/java/com/example/carebout/view/community/AddActivity.kt
@@ -183,8 +183,6 @@ class AddActivity: AppCompatActivity() {
         val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         intent.type = "image/*"
 
-        selectedImageUri = null
-
         requestGalleryLauncher.launch(galleryIntent)
     }
 }
